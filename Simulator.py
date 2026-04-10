d8f0cda954e2e837c1ef1fa41f509f58f6acaf41
kumud25279-cyber <kumud25279@iiitd.ac.in>
2026-04-10 20:11:22 +0530

    Add load and execute functions for instruction handling

--- Simulator.py
+++ Simulator.py
@@ -73,3 +73,75 @@ def write_mem(addr,val):
         if(0<=idx<32):
             data_mem[idx]=val
 
+#LOAD
+def load(filepath):
+    addr=0
+    with open(filepath) as f:
+        for line in f:
+            line =line.strip()
+            if line:
+                if(addr % 4 != 0):
+                    print(f"Error: Program counter {hex(addr)} is not divisible by 4. Breaking load.")
+                    break
+
+                # each line is an one 32-bit instruction in binary
+                program[addr]=int(line,2)
+                addr+=4
+
+#EXECUTE
+def execute():
+    global pc
+
+    if pc not in program:
+        return False
+
+    ins =program[pc]
+    opcode=ins&0x7F
+
+    #R TYPE
+    if(opcode==0b0110011):
+        rd=(ins>>7)&0x1F
+        f3=(ins>>12)&0x07
+        rs1=(ins>>15)&0x1F
+        rs2=(ins>>20)&0x1F
+        f7=(ins>>25)&0x7F
+
+        a,b=s32(regs[rs1]),s32(regs[rs2])
+        ua,ub=u32(regs[rs1]),u32(regs[rs2])
+
+        if(f3==0 and f7==0):
+            write_reg(rd,a+b)
+        elif(f3==0 and f7==0b0100000):
+            write_reg(rd,a-b)
+        elif(f3==1):
+            write_reg(rd,ua<<(ub&31))
+        elif(f3==2):
+            write_reg(rd,1 if a<b else 0)
+        elif(f3==3):
+            write_reg(rd,1 if ua<ub else 0)
+        elif(f3==4):
+            write_reg(rd,ua^ub)
+        elif(f3==5 and f7==0):
+            write_reg(rd,ua>>(ub&31))
+        elif(f3==5 and f7==0b0100000):
+            write_reg(rd,a>>(ub&31))
+        elif(f3==6):
+            write_reg(rd,ua|ub)
+        elif(f3==7):
+            write_reg(rd,ua&ub)
+
+        pc+=4
+
+    #I TYPE (addi, sltiu)
+    elif(opcode ==0b0010011):
+        rd=(ins>>7)&0x1F
+        f3=(ins>>12)&0x07
+        rs1=(ins>>15)&0x1F
+        imm=sext((ins>>20)&0xFFF,12)
+
+        if(f3==0):      # addi
+            write_reg(rd,s32(regs[rs1])+imm)
+        elif(f3 ==3):    # sltiu
+            write_reg(rd,1 if u32(regs[rs1])<u32(imm) else 0)
+
+        pc+=4
